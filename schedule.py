06c036ed942c6b001df5efab832a12beed6b3bad
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-08-05 12:11:57 +0200

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -4,7 +4,7 @@ import time
 def job():
     print("I'm working...")
 
-schedule.every(10).seconds.do(job)
+schedule.every(1).seconds.do(job)
 #schedule.every(10).minutes.do(job)
 #schedule.every().hour.do(job)
 #schedule.every().day.at("10:30").do(job)
